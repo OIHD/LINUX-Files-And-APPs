f7bb49567e26e715b9679398b810a48690ddc7ba
Göktuğ <oihdofficial@outlook.com>
2022-05-19 07:58:11 +0300

    Create geciciflax.sh

---
+++ codes/sh/flax/geciciflax.sh
@@ -0,0 +1,5 @@
+cd /home/oihd/app/flaxengine/flaxprojeler/
+rm -rf gecici
+mkdir gecici
+cd $HOME/app/flaxengine/FlaxEditorLinux/Binaries/Editor/Linux/Development
+./FlaxEditor -new -project "/home/oihd/app/flaxengine/flaxprojeler/gecici/" -std -nvidia
