836f9e02ca11a7197809b4b515860736fb70e1cf
Göktuğ <oihdofficial@outlook.com>
2022-05-19 08:04:45 +0300

    Update quick-lite.sh

--- app/quick/quick-lite.sh
+++ app/quick/quick-lite.sh
@@ -7,7 +7,13 @@ cd $QUICKFOLDER
 chmod +x quick-apps.sh quick-firefox-restore.sh quick-lite.sh quick-theme-light.sh
 sh quick-apps.sh
 sh quick-firefox-restore.sh
-sudo apt --fix-broken install
-echo -e "- ${RED}Complete - Press enter to exit "
-read goodbyemyfriend
+sudo apt --fix-broken install -y
+sudo apt-get install -y curl libcurl4-gnutls-dev mono-devel
+echo -e "${NC} FLAX ENGINE - installing"
+cd $HOME/app/flaxengine
+unzip FlaxEditorLinux.zip -d $HOME/app/flaxengine/FlaxEditorLinux
+mkdir flaxprojeler
+cd flaxprojeler
+mkdir proje
+cd $HOME/app/flaxengine/FlaxEditorLinux/Binaries/Editor/Linux/Development ;./FlaxEditor -new -project $HOME/app/flaxengine/flaxprojeler/proje 
 exit
