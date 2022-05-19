c4981f9644e613883ba62df17bde15c6cce86b96
Göktuğ <oihdofficial@outlook.com>
2022-05-19 19:32:35 +0300

    Create hgitflax.sh

---
+++ app/quick/flax/hgitflax.sh
@@ -0,0 +1,4 @@
+cd /home/oihd/app/flaxengine/flaxprojeler/gitproje/unnamed/
+rm -rf Cache
+cd $HOME/app/flaxengine/FlaxEditorLinux/Binaries/Editor/Linux/Development
+./FlaxEditor -project "/home/oihd/app/flaxengine/flaxprojeler/gitproje/unnamed/proje.flaxproj" -std -nvidia
