ca600243f1f1876f1799c770b2989c2497237502
Göktuğ <oihdofficial@outlook.com>
2022-07-19 16:17:37 +0300

    Create info.cpp

---
+++ codes/cpp/info.cpp
@@ -0,0 +1,19 @@
+/*
+main  // function 
+{
+statement ; // ";" end of statement 
+}
+
+int a ; //value initialization , // no initializer
+int a {} ; // initializer after equals sign
+
+a= 5 //copy assignment of value 5 into variable width [ Copy initialization ]
+
+() // Direct initialization 
+{} // Brace initialization
+
+What is the difference between initialization and assignment?
+Initialization gives a variable an initial value at the point when it is created. Assignment gives a variable a value at some point after the variable is created.
+
+
+*/
